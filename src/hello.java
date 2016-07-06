20e648e339c2ed4f7241d1805523516a0892b9a9
abin <abininbox@gmail.com>
2016-07-06 17:10:45 +0530

    First program in Java Hello World, I'm in

---
+++ src/hello.java
@@ -0,0 +1,8 @@
+/**
+ * Created by abin on 6/7/16.
+ */
+public class hello {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/hellop.java
@@ -0,0 +1,8 @@
+/**
+ * Created by abin on 6/7/16.
+ */
+public class hellop {
+    public static void main(String[] args){
+        System.out.println("Hello Peppal");
+    }
+}
