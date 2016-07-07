0b563cb5c3002ea443b88eec01a91293eedf04f3
abin <abininbox@gmail.com>
2016-07-07 23:48:30 +0530

    variables char boolean

---
+++ src/hek.java
@@ -0,0 +1,12 @@
+/**
+ * Created by abin on 7/7/16.
+ */
+public class hek {
+    public static void main(String[] ken) {
+        char njek = 'K';
+        boolean yes=true;
+        boolean no=false;
+        System.out.println(njek);
+        System.out.println(no);
+    }
+}
